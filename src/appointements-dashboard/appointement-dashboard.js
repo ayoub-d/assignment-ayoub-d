58ea9f802f725d68451313d2a80120aed14ead86
ayoub-d <ayoub.deqqaq@gmail.com>
2020-11-24 03:50:14 +0100

    refactored create appointement component, fixed validation issues

--- src/appointements-dashboard/appointement-dashboard.js
+++ src/appointements-dashboard/appointement-dashboard.js
@@ -1,64 +1,16 @@
-import React, { Children } from "react";
 import "./style.css";
 import { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
-import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { uid } from "uid";
 import AppointementItem from "../appointement-item/appointement-item";
 import { filter } from "lodash";
+import "react-datepicker/dist/react-datepicker.css";
+import CreateAppointementModal from "../create-appointement-modal/create-appointement-modal";
 
 const localizer = momentLocalizer(moment);
-
-function AddAppointementModal({
-  showModal,
-  onCloseModal,
-  onAppointementAdded,
-}) {
-  const [appointementTitle, setAppointementTitle] = useState();
-  return (
-    <>
-      <Modal show={showModal}>
-        <Modal.Header>Create Appointement</Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="formBasicAppointementTitle">
-              <Form.Label>Appointement Title</Form.Label>
-              <Form.Control
-                placeholder="Enter Appointement Title"
-                onChange={(event) => {
-                  setAppointementTitle(event.target.value);
-                }}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="success"
-            onClick={() => {
-              const appointement = {
-                id: uid(16),
-                title: appointementTitle,
-                allDay: true,
-                start: new Date(),
-                end: new Date(),
-              };
-              onAppointementAdded(appointement);
-              onCloseModal();
-            }}
-          >
-            Create Appointement
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
 
 function AppointementsDashboard() {
   const [showAddAppointementModal, setShowAddAppointementModal] = useState(
@@ -84,9 +36,9 @@ function AppointementsDashboard() {
             variant="success"
             onClick={() => setShowAddAppointementModal(true)}
           >
-            Add Appointement
+            Create Appointement
           </Button>
-          <AddAppointementModal
+          <CreateAppointementModal
             showModal={showAddAppointementModal}
             onCloseModal={() => {
               setShowAddAppointementModal(false);
---
+++ src/create-appointement-modal/create-appointement-modal.js
@@ -0,0 +1,98 @@
+import DatePicker from "react-datepicker";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { uid } from "uid";
+import { useState } from "react";
+
+function CreateAppointementModal({
+  showModal,
+  onCloseModal,
+  onAppointementAdded,
+}) {
+  const [appointementTitle, setAppointementTitle] = useState();
+  const [appointementDate, setAppointementDate] = useState();
+  const [errors, seterrors] = useState([]);
+  const validateAppointement = () => {
+    const validattionErrors = [];
+    if (!appointementTitle) {
+      validattionErrors.push("An appointement must have a title");
+    }
+    if (!appointementDate) {
+      validattionErrors.push("An appointement must have a date");
+    }
+    seterrors(validattionErrors);
+  };
+  return (
+    <>
+      <Modal show={showModal}>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicAppointementTitle">
+              <Form.Label>Appointement Title</Form.Label>
+              <Form.Control
+                placeholder="Enter Appointement Title"
+                onChange={(event) => {
+                  setAppointementTitle(event.target.value);
+                }}
+                onBlur={() => validateAppointement()}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicAppointementDate">
+              <Form.Label style={{ marginRight: 10 }}>
+                Appointement Date
+              </Form.Label>
+              <DatePicker
+                showMonthDropdown
+                dropdownMode="select"
+                todayButton="Today"
+                selected={appointementDate}
+                onChange={(value) => {
+                  setAppointementDate(value);
+                }}
+                onBlur={() => validateAppointement()}
+                value={appointementDate}
+              />
+            </Form.Group>
+            <div className="add-appointement-errors">
+              {errors.map((error) => (
+                <div className="error">{error}</div>
+              ))}
+            </div>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+          variant="secondary"
+            onClick={() => {
+              onCloseModal();
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant={!!errors.length ? "disabled" : "success"}
+            disabled={!!errors.length}
+            onClick={() => {
+              const appointement = {
+                id: uid(16),
+                title: appointementTitle,
+                allDay: true,
+                start: appointementDate,
+                end: appointementDate,
+              };
+              onAppointementAdded(appointement);
+              onCloseModal();
+              setAppointementDate(undefined);
+              setAppointementTitle(undefined);
+            }}
+          >
+            Create Appointement
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default CreateAppointementModal;
